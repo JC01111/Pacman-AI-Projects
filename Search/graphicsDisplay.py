14a2842fc39287e8f8e46ccffc8e64b31aaa2577
Jay Chen <jaychenCJW0920@gmail.com>
2024-06-11 14:37:56 -0400

    Upload

--- Search/graphicsDisplay.py
+++ Search/graphicsDisplay.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -262,7 +262,7 @@ class PacmanGraphics:
         begin_graphics(screen_width,
                        screen_height,
                        BACKGROUND_COLOR,
-                       "CS188 Pacman")
+                       "Pacman")
 
     def drawPacman(self, pacman, index):
         position = self.getPosition(pacman)
